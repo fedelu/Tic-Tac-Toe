bbc911bfe3c30661d1bd25e55db2a1872fe5fbde
Federico Delucchi <federicodelucchivona@gmail.com>
2021-11-15 09:47:15 -0300

    arreglos fondos

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+$(document).ready(function(){
+     var tema =$("#tema");
+     $("#a-verde").click(function(){
+         tema.attr("href","css/verde.css");
+     });
+     $("#a-rojo").click(function(){
+         tema.attr("href","css/rojo.css");
+     });
+     $("#a-azul").click(function(){
+         tema.attr("href","css/azul.css");
+     });
+ });
 var playerTurn, moves, isGameOver, span, restartButton;  
 playerTurn = "x";  
 moves = 0;  
